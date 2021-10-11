29125d9f0f53ffaee5ac3ebc871948225491edde
kaoutar zaia <kaoutarZaia@gmail.com>
2021-10-11 13:23:01 -0400

    Create Tower_of_hanoi.py

---
+++ ch8-recursion/kaoutar/Tower_of_hanoi.py
@@ -0,0 +1,11 @@
+"""
+
+In the classic problem of the Towers of Hanoi, you have 3 towers and N disks of different sizes which can slide onto any tower. 
+The puzzle starts with disks sorted in ascending order of size from top to bottom (Le., each disk sits on top of an even larger one).
+You have the following constraints:
+
+Only one disk can be moved at a time.
+A disk is slid off the top of one tower onto another tower.
+A disk cannot be placed on top of a smaller disk.
+Write a program to move the disks from the first tower to the last using stacks. https://en.wikipedia.org/wiki/Tower_of_Hanoi
+"""
